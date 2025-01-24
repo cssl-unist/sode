7dd4423ce64a73d7b539de384224b6b33c43a3aa
chanyoung <blazer502@unist.ac.kr>
2025-01-24 12:24:54 +0900

    Explicitly calling python version

--- eval/figure.sh
+++ eval/figure.sh
@@ -2,33 +2,33 @@
 
 mkdir -p figure/data/
 
-python parser.py
+python3 parser.py
 
 cd figure
-python fig5_a.py
-python fig5_b.py
-python fig5_c.py
-python fig5_d.py
+python3 fig5_a.py
+python3 fig5_b.py
+python3 fig5_c.py
+python3 fig5_d.py
 
-python fig6_a.py
-python fig6_b.py
-python fig6_c.py
+python3 fig6_a.py
+python3 fig6_b.py
+python3 fig6_c.py
 
-python fig7_a.py
-python fig7_b.py
+python3 fig7_a.py
+python3 fig7_b.py
 
-python fig8_a.py
-python fig8_b.py
+python3 fig8_a.py
+python3 fig8_b.py
 
-python fig9.py
+python3 fig9.py
 
-python fig10.py
+python3 fig10.py
 
-python fig11.py
+python3 fig11.py
 
-python fig12.py
+python3 fig12.py
 
-python fig13.py
+python3 fig13.py
 
-python appendix_fig2.py
+python3 appendix_fig2.py
 cd ..
